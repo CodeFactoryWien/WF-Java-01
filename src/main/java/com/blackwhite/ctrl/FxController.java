e3d618173bdf5a23d00b94e68f960360bf04d1b8
SG <web.sg@protonmail.com>
2020-01-16 11:15:28 +0100

    fixed issue with fxml tags

--- src/main/java/com/blackwhite/ctrl/FxController.java
+++ src/main/java/com/blackwhite/ctrl/FxController.java
@@ -1,4 +1,11 @@
 package com.blackwhite.ctrl;
 
+import javafx.geometry.Insets;
+import javafx.scene.web.WebView;
+
 public class FxController {
+    public WebView webview;
+    public Insets x4;
+    public Insets x1;
+    public Insets x5;
 }
